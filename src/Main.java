05efff80caccdebbde3e99240494bdcf68515069
shoniaaaa <shoniaardhy@gmail.com>
2025-10-21 09:42:54 +0700

    Update after commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        String version = "v1.1";
+        System.out.println("Hello GitHub! - " + version);
+        System.out.println("Kode ini merupakan update terbaru untuk commit selanjutnya.");
     }
 }
